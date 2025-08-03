3673b5a37987b2d1199215cf2cb9af390d05488c
rdngobeni <rivo.ngobeni@standardbank.co.za>
2025-08-03 15:33:58 +0200

    fix infra

--- terraform/mongodb.tf
+++ terraform/mongodb.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    mongodbatlas = {
-      source  = "mongodb/mongodbatlas"
-      version = "1.15.0"
-    }
-  }
-}
-
 provider "mongodbatlas" {
   public_key  = var.atlas_public_key
   private_key = var.atlas_private_key
