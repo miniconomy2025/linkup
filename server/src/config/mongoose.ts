50f641869354847e6c2680df6075e6df3f29b2c1
rdngobeni <rivo.ngobeni@standardbank.co.za>
2025-08-04 17:04:02 +0200

    remove log

--- server/src/config/mongoose.ts
+++ server/src/config/mongoose.ts
@@ -6,9 +6,6 @@ export const connectMongoDB = async (): Promise<typeof mongoose> => {
   if (isConnected) 
     return mongoose;
 
-  console.log('Current working dir:', process.cwd());
-  console.log('Using Mongo URI:', process.env.MONGO_URI);
-
   const uri = process.env.MONGO_URI!;
 
   await mongoose.connect(uri, {
