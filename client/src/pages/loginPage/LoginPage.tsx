f2833c4924a959d1f054516939389fc599402d1e
Chris McHardy <chris.mchardy@bbd.co.za>
2025-08-06 17:36:05 +0200

    Update the client ID

--- client/src/pages/loginPage/LoginPage.tsx
+++ client/src/pages/loginPage/LoginPage.tsx
@@ -9,7 +9,7 @@ export const LoginPage: React.FC = () => {
         // local - http://localhost:3000/auth/google/callback
         // production - http://ec2-13-244-78-172.af-south-1.compute.amazonaws.com/api/auth/google/callback
 
-        const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '931664523187-tttf6k218o12ioinl0cb19rqkebcu52r.apps.googleusercontent.com';
+        const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '931664523187-6a5itlf9unh9u4oohkcjv5s5c9b94a97.apps.googleusercontent.com';
         const redirectUri = import.meta.env.VITE_GOOGLE_REDIRECT_URI || 'http://ec2-13-244-78-172.af-south-1.compute.amazonaws.com/api/auth/google/callback';
         const scope = 'openid profile email';
         const url =
