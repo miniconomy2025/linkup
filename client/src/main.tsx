20b5f4ad33331eb02fec4b4890be5d11adf4e5b3
Chris McHardy <chris.mchardy@bbd.co.za>
2025-08-06 18:18:17 +0200

    Update vite env's in workflow

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,9 +8,11 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 // Local - 931664523187-tttf6k218o12ioinl0cb19rqkebcu52r.apps.googleusercontent.com
 // Production - 931664523187-6a5itlf9unh9u4oohkcjv5s5c9b94a97.apps.googleusercontent.com
 
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '931664523187-tttf6k218o12ioinl0cb19rqkebcu52r.apps.googleusercontent.com';
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <GoogleOAuthProvider clientId="931664523187-6a5itlf9unh9u4oohkcjv5s5c9b94a97.apps.googleusercontent.com">
+        <GoogleOAuthProvider clientId={clientId}>
             <AuthProvider>
                 <App />
             </AuthProvider>
