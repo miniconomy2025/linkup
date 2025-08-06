613257997c05da913a7e3823da476954d6d21243
Chris McHardy <chris.mchardy@bbd.co.za>
2025-08-06 17:53:34 +0200

    Update the frontend url to point to backend

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://ec2-13-244-78-172.af-south-1.compute.amazonaws.com/api/api';
 
 export async function apiFetch<T>(
     path: string,
